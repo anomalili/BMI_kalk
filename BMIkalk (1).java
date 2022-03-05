88fc8622ff4dfbf595ff568c124f2b31bce2f0aa
mzsl <95681537+anomalili@users.noreply.github.com>
2022-03-05 19:57:19 +0100

    Update BMIkalk (1).java

--- BMIkalk (1).java
+++ BMIkalk (1).java
@@ -49,14 +49,11 @@ public class BMIkalk {
     static void ertekel(double magassagcm, double tomegkg, double BMI) {
 //        System.out.println("A testmagassság "+ magassagcm + " cm, a testtömeg " + tomegkg +" kg ez alapján a BMI: " + Math.round(BMI) + " .");
         System.out.println("A testmagassság "+ magassagcm + " cm, a testtömeg " + tomegkg +" kg ez alapján a BMI: " + String.format("%.2f", BMI));
-        if (BMI < 16) {System.out.println("Ön súlyosan sovány");}
-        if (BMI > 16 && BMI < 16.99) {System.out.println("Ön súlyosan sovány");}
-        if (BMI > 17 && BMI < 18.49) {System.out.println("Ön mérsékelten sovány");}
-        if (BMI > 18.5 && BMI < 24.99) {System.out.println("Ön enyhén sovány");}
-        if (BMI < 29.99 && 25 < BMI) {System.out.println("Ön normális");}
-        if (BMI < 34.99 && BMI > 30) {System.out.println("Ön I. fokú túlsúlyos");}
-        if (BMI < 39.99 && 35 < BMI) {System.out.println("Ön II. fokú túlsúlyos");}
-        if (BMI > 40) {System.out.println("Ön III. fokú túlsúlyos");}
+      if (BMI < 20) {System.out.println("sovány");}
+        if (BMI > 20 && BMI < 25) {System.out.println("normál");}
+        if (BMI > 25 && BMI < 30) {System.out.println("túlsúlyos");}
+        if (BMI > 30 && BMI < 35) {System.out.println("elhízott");}
+        if (BMI > 35) {System.out.println("kórosan elhízott");}
     }
 }
     
